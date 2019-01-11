78037582af16ce34f39f4d5447a442e0ea73f39b
hannahwynnjones <hannah.wynn-jones@gtb.com>
2019-01-11 10:38:53 +0000

    with new bits

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, request, redirect
+import pymongo
+from pymongo import MongoClient
+MONGO_URL = os.environ.get('mongohq-transparent-95617')
+client = MongoClient(MONGO_URL)
+
+db = client.app121630054
+collection = db.shoutouts
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello world!"
+@app.route("/", methods=['GET'])
+def index():
+    shouts = collection.find()
+    return render_template('index.html', shouts=shouts)
+
+@app.route("/post", methods=['POST'])
+def post():
+    shout = {"name":request.form['name'], "message":request.form['message']}
+    shout_id = collection.insert(shout)
+    return redirect('/')
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
